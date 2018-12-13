6028bf2bd882c63333dbff7e6825b610e68bb8d3
Ingmar Dasseville <ingmar.dasseville@cs.kuleuven.be>
2018-12-13 18:09:24 +0100

    add primeng for ui and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {IdpService} from '../services/idp.service';
 import {HttpClientModule} from '@angular/common/http';
+import {SliderModule} from 'primeng/slider';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -11,7 +14,10 @@ import {HttpClientModule} from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    SliderModule
   ],
   providers: [IdpService],
   bootstrap: [AppComponent]
